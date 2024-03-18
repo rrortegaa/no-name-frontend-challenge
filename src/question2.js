07d9121a7c5db7b560032da75b9bdcba66b7c440
Rose <rrortegaa@gmail.com>
2024-03-18 02:48:17 -0600

    :hammer: second part added and finished

---
+++ src/question2.js
@@ -0,0 +1,6 @@
+const button = document.getElementById('change-bg')
+const text = document.getElementById('second-question-text-bg')
+
+button.addEventListener('click', () => {
+  text.style.backgroundColor = '#f78ad8'
+})
---
+++ src/question3.js
@@ -0,0 +1,11 @@
+
+function getMax(numbers = []) {
+  let max = numbers[0]
+
+  for(let i= 0; i < numbers.length; i++) {
+    if(number[i] > max) {
+      max = number[i]
+    }
+  }
+  return max
+}
